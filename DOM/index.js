495a1600b780255a5a372c6f8a27e52fe72d5e94
Ankur-Sh <ankursharma.as0403@gmail.com>
2022-09-28 00:42:00 +0530

    Dice website and Drum kit starting

--- DOM/index.js
+++ DOM/index.js
@@ -1,7 +1,15 @@
 document.querySelector("h1").innerHTML = "Good Bye";
+//document.querySelector("h1").textContent = "Good Bye";-->
+//We can change text inside it but it is not a html so we cant write html in it
+
 var heading =
     document.firstElementChild.lastElementChild.lastElementChild
         .lastElementChild;
+document.querySelector("a").getattribute("href");
+document.querySelector("a").setattribute("href", "https://www.bing.com");
+document.querySelector("h1").classList.add("huge"); //we also can use remove and toggle
+//where toggle is to if added than remove and if removed then add class.
+
 document.querySelector("#list li a").style.color = "blue";
 document.querySelector("button").style.backgroundColor = "yellow";
 document.getElementsByTagName("li")[2].style.color = "purple";
